d6a958970d9d0aef664f40836af49727093fc6a2
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-05 00:34:48 +0530

    Print Text With Number

---
+++ 01_input_output/PrintTextWithNumber.java
@@ -0,0 +1,13 @@
+/*
+WAP: Print Text With Number
+Date: 05-Dec-2025
+*/
+
+public class PrintTextWithNumber {
+    public static void main(String[] args){
+        int x = 2, y = 3;
+        System.out.println("Addition of "+x+" and "+y+" is: "+(x+y));
+        int add = x+y;
+        System.out.print("Addition: "+add);
+    }
+}
