fd08b2a40fee122f04c0fa818a822708c03fb064
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-05 00:24:19 +0530

    variables and their rules

---
+++ 01_input_output/Variables.java
@@ -0,0 +1,26 @@
+/*
+WAP: Variables
+Date: 05-Dec-2025
+*/
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 5;
+        // Here x is variable, variable is type of container which is used to store or hold some values.
+        System.out.println(x);
+        int y = 6;
+        System.out.println(y);
+        System.out.println(x+y);
+        x = x-4;
+        System.out.println(x);
+    }
+}
+
+
+/*
+Few Rules of Creating Variables
+1. V name can start from alphabets, _(underscore) or $.
+2. V name cannot start's from numbers and any other types of special characters except _ or $.
+3. V name cannot be start's reversed keywords (49 reserved keyword).
+4. Try to create V name meaningful.
+ */
