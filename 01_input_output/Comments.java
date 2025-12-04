155efcf506c5451b5a2a3e2b0e0e32bccdee4d36
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-05 00:15:00 +0530

    comments and their types

---
+++ 01_input_output/Comments.java
@@ -0,0 +1,22 @@
+public class Comments {
+    public static void main(String[] args){
+        System.out.print("Today, i learn what is comments and their type!");
+    }
+}
+
+
+//Comments - is use to write some messages in the program, which other people's can read and understand the program.
+//           It is not consider or ignore during compilation process by complier.
+//           There are three types of comments:-
+
+//1. Single line comment - use add single line comment!
+
+/*
+2. Multiline comment - use to add mutiple lines comments!
+Author: Yuvraj Kumar Singh.
+Date: 05-Dec-2025
+*/
+
+/**
+3. documentation Comment - used to include documentation part in the program.
+*/
