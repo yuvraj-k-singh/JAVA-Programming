f5ce475dedca8b5c15cfab0393bd2cc3f8eb4567
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-04 23:59:42 +0530

    a

--- 01_input_output/Newline.java
+++ 01_input_output/Newline.java
@@ -1,6 +1,6 @@
 public class Newline {
     public static void main(String[] args) {
-        // println is used to print new line after printing current line.
+        // println is used to change the line after printing current line.
         System.out.println("My name is Yuvraj kumar singh");
         // \n is used to change the line just like we did in c programming.
         System.out.print("I did my schooling from M.G.M School.\nCurrently i'm persuing my BTECH from IIST college.");
---
+++ 01_input_output/PrintingNumber.java
@@ -0,0 +1,13 @@
+public class PrintingNumber {
+    public static void main(String[] args){
+        //when we put some numbers in double cots "", it consider as string.
+        System.out.println("4");
+        System.out.println("4+5");
+
+        System.out.println();
+
+        //when we put some numbers without double cots "", it is consider as numberic value.
+        System.out.println(5);
+        System.out.println(4+5);
+    }
+}
