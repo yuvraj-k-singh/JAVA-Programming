a5a1df92c4de0c9143ae29d92351ca10f28e5f90
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-07 22:15:39 +0530

    a

---
+++ 01_input_output/SumOfTwo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SumOfTwo {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the value of b: ");
+        int b = sc.nextInt();
+        System.out.print("The sum of "+ a + " and " + " is: " + (a+b));
+    }
+}
