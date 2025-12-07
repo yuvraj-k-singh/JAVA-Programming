14f7b7f74869e3f265f26721c87ffab4e2c2b4ab
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-07 22:08:25 +0530

    operators

---
+++ 01_input_output/Operators.java
@@ -0,0 +1,18 @@
+/*
+WAP: Operators
+Date: 07-Dec-2025
+*/
+
+public class Operators {
+    public static void main(String[] args){
+
+        //Operators are used to perform operations on values or variable, there 7 main types of operators.
+
+        //1. Arithmetical operators = /,*,+,- (used to perform basic operations)
+        //2. unary operators = ++, -- (used to value by one)
+        //3. comparison operators = ==, <, >, <=, >=  (used to compare two values)
+        //4. logical operators == &&, ||, !    (used to perform logical operations)
+        //5. assignment operators = =, *=, /=, +=, -=   (used to assign values)
+        //6. bitwise operators = <<, >>, &, !, |  (used to perform bitwise operations on binary numbers).
+    }
+}
