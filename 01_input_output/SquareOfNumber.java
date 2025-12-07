d2ed863b79f46e9d02954271ee36b3622a23ef42
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-07 22:12:12 +0530

    Square Of Number

---
+++ 01_input_output/SquareOfNumber.java
@@ -0,0 +1,15 @@
+/*
+WAP: Square Of Number
+Date: 07-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class SquareOfNumber {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of number: ");
+        int n = sc.nextInt();
+        System.out.print("The Sqaure of " + n + " is: " + (n*n));
+    }
+}
