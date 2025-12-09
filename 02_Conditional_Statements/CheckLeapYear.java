2405ca0558011a6f1c19fd0c64d87f5c412548d3
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-09 06:30:32 +0530

    Check Leap Year

---
+++ 02_Conditional_Statements/CheckLeapYear.java
@@ -0,0 +1,21 @@
+/*
+WAP: Check Leap Year
+Date: 07-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class CheckLeapYear {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        short n = sc.nextShort();
+
+        if((n%4==0 && n%100!=0) || n%400==0){
+            System.out.print("The given year "+n+" is Leap Year!");
+        } else{
+            System.out.print("The given year "+n+" is not Leap Year!");
+        }
+    }
+}
