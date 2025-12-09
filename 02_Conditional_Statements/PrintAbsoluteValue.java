6b9f421739428c3604369a1cb864de12b98a84e5
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-09 06:38:18 +0530

    Print Absolute Value

--- 02_Conditional_Statements/PrintAbsoluteValue.java
+++ 02_Conditional_Statements/PrintAbsoluteValue.java
@@ -1,7 +1,21 @@
 /*
 WAP: Print Absolute Value
-Date: 07-Dec-2025
+Date: 09-Dec-2025
 */
 
+import java.util.Scanner;
+
 public class PrintAbsoluteValue {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if(n>0){
+            System.out.print("The Absolute Value of "+n+" is: "+ n);
+        } else{
+            System.out.print("The Absolute Value of "+n+" is: "+ (n*-1));
+        }
+    }
 }
