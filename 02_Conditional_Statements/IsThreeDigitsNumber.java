5d677ce89607e30e49eeebf7bf2e798c53cf701f
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-10 06:40:26 +0530

    Is Three Digits Number

---
+++ 02_Conditional_Statements/IsThreeDigitsNumber.java
@@ -0,0 +1,22 @@
+/*
+WAP: Is Three Digits Number
+Date: 09-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class IsThreeDigitsNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a three digit number: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        if(n>99 && n<1000){
+            System.out.print("The given number "+n+" is three digit number!");
+        } else{
+            System.out.print("The given number "+n+" is not three digit number!");
+        }
+    }
+}
