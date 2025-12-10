0b3cf1eede8f8aa6e20568cd1dac336d56cf98b8
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-10 23:21:57 +0530

    Compare Int And Char

---
+++ 02_Conditional_Statements/CompareIntAndChar.java
@@ -0,0 +1,16 @@
+/*
+WAP: Compare Int And Char
+Date: 10-Dec-2025
+*/
+
+
+public class CompareIntAndChar {
+    public static void main(String[] args){
+        int x = 65;
+        char ch = 'A';
+
+        if(x==ch){
+            System.out.print("Int and Char values are equal!");
+        } else System.out.print("Int and Char values are not equal!");
+    }
+}
