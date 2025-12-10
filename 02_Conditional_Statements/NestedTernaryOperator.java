5416442d62b76c79a58f845c0ed0c3b75ebbf506
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-11 00:26:11 +0530

    Nested Ternary Operator

--- 02_Conditional_Statements/NestedTernaryOperator.java
+++ 02_Conditional_Statements/NestedTernaryOperator.java
@@ -1,3 +1,21 @@
-public class NestedTernaryOperator {
+/*
+WAP: Nested Ternary Operator
+Date: 11-Dec-2025
+*/
 
+
+import java.util.Scanner;
+
+public class NestedTernaryOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the three different values: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println();
+        System.out.print(((a>b) ? ((a>c) ? "A is greater number!" : "C is greater number!") : ((b>c) ? "B is greater number!" : "C is greater number!")));
+    }
 }
