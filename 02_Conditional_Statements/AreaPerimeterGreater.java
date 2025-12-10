0dcae829738da7255ddb6f1ac7dbba1de3c88a7f
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-10 06:29:47 +0530

    Check Profit Or Loss

---
+++ 02_Conditional_Statements/AreaPerimeterGreater.java
@@ -0,0 +1,25 @@
+/*
+WAP: Check Area Perimeter Greater
+Date: 09-Dec-2025
+*/
+
+
+import java.util.Scanner;
+
+public class AreaPerimeterGreater {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Length of Rectangle: ");
+        double length = sc.nextDouble();
+        System.out.print("Enter the Breadth of Rectangle: ");
+        double breadth = sc.nextDouble();
+
+        double area = length*breadth;
+        double perimeter = 2*(length+breadth);
+        System.out.println();
+        if(area>perimeter){
+            System.out.print("The area value "+area+" is greater than perimeter value "+perimeter+ "!");
+        } else System.out.print("The perimeter value "+perimeter+" value is greater than area value "+area+ "!");
+    }
+}
---
+++ 02_Conditional_Statements/CheckProfitOrLoss.java
@@ -0,0 +1,29 @@
+/*
+WAP: Check Profit Or Loss
+Date: 09-Dec-2025
+*/
+
+
+import java.util.Scanner;
+
+public class CheckProfitOrLoss {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the cost price: ");
+        double cp = sc.nextDouble();
+
+        System.out.print("Enter the selling price: ");
+        double sp = sc.nextDouble();
+
+        System.out.println();
+        double check = sp - cp;
+        if(check>0){
+            System.out.print("The seller has made profit of amount: "+check);
+        } else if(check == 0){
+            System.out.print("The seller has made no loss and profit!");
+        } else{
+            System.out.print("The seller has made loss of amount: "+ (check*-1));
+        }
+    }
+}
