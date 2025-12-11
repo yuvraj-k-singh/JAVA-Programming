8eecf6b740167b5196fb642efdc6589257749e7e
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-12 00:23:47 +0530

    Basic For Loop

---
+++ 03_Loops/BasicForLoop.java
@@ -0,0 +1,15 @@
+/*
+WAP: Basic For Loop
+Date: 12-Dec-2025
+*/
+
+
+public class BasicForLoop {
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.println("Hello World!");
+        }
+    }
+}
+
+
