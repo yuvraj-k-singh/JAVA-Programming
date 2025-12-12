169427f84576ae4636b0d12273bb1c7af538a108
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-12 07:13:54 +0530

    Odd Numbers

---
+++ 03_Loops/OddNumbers.java
@@ -0,0 +1,22 @@
+/*
+WAP: Odd Numbers
+Date: 12-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class OddNumbers {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n numbers: ");
+        int n = sc.nextInt();
+
+        System.out.println("\nOdd numbers from 1 to "+n+" is: ");
+        for(int i=1; i<=n; i++){
+            if(i%2 != 0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
