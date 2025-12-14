dd0fd046eb55c87d4baf349d647f54e398806b66
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-14 18:24:02 +0530

    Product Of Digit Of Number

---
+++ 03_Loops/ProdcutOfDigitOfNumber.java
@@ -0,0 +1,23 @@
+/*
+WAP: Product Of Digit Of Number
+Date: 14-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class ProdcutOfDigitOfNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int temp = n, multi = 1;
+
+        while(n!=0){
+            multi *= n%10;
+            n /= 10;
+        }
+
+        System.out.print("\nThe multiplication of digit of "+temp+" number is: "+((multi>0) ? multi : -1*multi));
+    }
+}
