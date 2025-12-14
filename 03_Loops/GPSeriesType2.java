0e964e87144d2c869bf911d23923e4828cc80fe5
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-14 09:15:37 +0530

    GPSeriesType2

---
+++ 03_Loops/GPSeriesType2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GPSeriesType2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first term: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the common ratio: ");
+        int r = sc.nextInt();
+        System.out.print("Enter the no of terms: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.print(a+", ");
+            a *= r;
+        }
+    }
+}
