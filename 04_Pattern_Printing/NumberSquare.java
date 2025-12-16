b3f01578756310fe105a0971c38ef77b955abb89
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-16 06:20:45 +0530

    Number Square

---
+++ 04_Pattern_Printing/NumberSquare.java
@@ -0,0 +1,23 @@
+/*
+WAP: Number Square
+Date: 16-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class NumberSquare {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
