eb5c9482da4c45a1935e44d2f54cf316695cb0cc
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-16 06:06:25 +0530

    Star Square

---
+++ 04_Pattern_Printing/StarSquare.java
@@ -0,0 +1,23 @@
+/*
+WAP: Star Square
+Date: 16-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class StarSquare {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter no of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=rows; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
