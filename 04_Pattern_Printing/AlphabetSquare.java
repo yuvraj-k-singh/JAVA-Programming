6a6968836c4d335243e14fa0147da8e56b372b52
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-16 06:27:38 +0530

    Alphabet Square

---
+++ 04_Pattern_Printing/AlphabetSquare.java
@@ -0,0 +1,26 @@
+/*
+WAP: Alphabet Square
+Date: 16-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+        char ch;
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            ch = 'A';
+            for(int j=1; j<=n; j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
