2e00d9e5260481d483c9123bd1db61a1ec783445
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-17 05:45:35 +0530

    Star Triangle

---
+++ 04_Pattern_Printing/StarTraigle.java
@@ -0,0 +1,23 @@
+/*
+WAP: Star Triangle
+Date: 16-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class StarTraigle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
