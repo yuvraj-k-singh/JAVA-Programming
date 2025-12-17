79f1deb8eb3ad3e379a97b847a1643bfba53a227
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-17 05:58:32 +0530

    Alphabet Triangle Type2

---
+++ 04_Pattern_Printing/AlphabetTriangleType2.java
@@ -0,0 +1,23 @@
+/*
+WAP: Alphabet Triangle Type2
+Date: 17-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class AlphabetTriangleType2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print((char)(i+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
