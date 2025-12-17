d50106215e4eba437bd6b1e5b4bf38d576da8f99
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-17 23:44:27 +0530

    Binary Triangle

---
+++ 04_Pattern_Printing/BinaryTriangle.java
@@ -0,0 +1,32 @@
+/*
+WAP: Binary Triangle
+Date: 17-Dec-2025
+
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+
+*/
+
+import java.util.Scanner;
+
+public class BinaryTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2==0){
+                    System.out.print(1 + " ");
+                } else System.out.print(0 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
