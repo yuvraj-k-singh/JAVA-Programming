516854ad86974d8c06369430061c2faba56cdf65
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-17 06:28:13 +0530

    Number Horizontal Flipped Triangle

---
+++ 04_Pattern_Printing/NumberHorizontalFlippedTriangle.java
@@ -0,0 +1,23 @@
+/*
+WAP: Number Horizontal Flipped Triangle
+Date: 17-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class NumberHorizontalFlippedTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n+1-i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
