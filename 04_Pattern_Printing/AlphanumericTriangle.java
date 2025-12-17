885354c250dbd3ef93deb4ffd63f2b4b3570f29a
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-17 06:04:42 +0530

    Alphanumeric Triangle

---
+++ 04_Pattern_Printing/AlphanumericTriangle.java
@@ -0,0 +1,25 @@
+/*
+WAP: Alphanumeric Triangle
+Date: 17-Dec-2025
+*/
+
+import java.util.Scanner;
+
+public class AlphanumericTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if(i%2==0){
+                    System.out.print((char)(j+64) + " ");
+                } else System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
