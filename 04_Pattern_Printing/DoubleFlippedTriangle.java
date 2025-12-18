8c03e79fea803a02d93a589e9af53dfe8e42598c
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-18 23:49:41 +0530

    Double Flipped Triangle

---
+++ 04_Pattern_Printing/DoubleFlippedTriangle.java
@@ -0,0 +1,33 @@
+/*
+WAP: Double Flipped Triangle
+Date: 18-Dec-2025
+
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+
+*/
+
+import java.util.Scanner;
+
+public class DoubleFlippedTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            for(int k=1; k<i; k++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=n+1-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
