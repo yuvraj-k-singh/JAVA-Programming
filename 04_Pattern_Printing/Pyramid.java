daec67cce26953a8424d4971f0d41fe50fa8b5d2
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-18 23:58:21 +0530

    Pyramid

---
+++ 04_Pattern_Printing/Pyramid.java
@@ -0,0 +1,32 @@
+/*
+WAP: Pyramid
+Date: 18-Dec-2025
+
+      *
+    * * *
+  * * * * *
+* * * * * * *
+
+*/
+
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=n; i++){
+            for(int k=n-1; k>=i; k--){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
