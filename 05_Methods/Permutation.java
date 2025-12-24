88671d17650d1afb3daed10119c5d8d210df36e3
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2025-12-24 21:05:23 +0530

    Permutation

--- 05_Methods/Permutation.java
+++ 05_Methods/Permutation.java
@@ -1,6 +1,19 @@
+/*
+WAP: Permutation
+Date: 24-Dec-2025
+*/
+
 import java.util.Scanner;
 
 public class Permutation {
+    public static int fact(int n){
+        int f = 1;
+        for(int i=1; i<=n; i++){
+            f *= i;
+        }
+        return f;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -11,6 +24,6 @@ public class Permutation {
 
         int npr = fact(n)/fact(n-r);
 
-        System.out.printf("\nThe permutation of %d");
+        System.out.printf("\nThe permutation of %dN and %dR is: %d", n, r, npr);
     }
 }
