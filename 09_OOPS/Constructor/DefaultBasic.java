2d4f3473610b3a33575aefafe9718320b4869ba2
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-03-10 23:31:54 +0530

    Default Constructor Basic

---
+++ 09_OOPS/Constructor/DefaultBasic.java
@@ -0,0 +1,18 @@
+/*
+WAP: Default Constructor Basic
+Date: 10-Mar-2026
+*/
+
+package Constructor;
+
+public class DefaultBasic {
+    public static void main(String[] args){
+        DefaultConstructor d1 = new DefaultConstructor();
+    }
+}
+
+class DefaultConstructor{
+    DefaultConstructor(){
+        System.out.println("This is DefaultConstructor");
+    }
+}
