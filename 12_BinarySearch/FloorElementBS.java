bb849724f23133cc8eff5c5a9dbdb6b63e3e8242
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-05-09 10:47:06 +0530

    Floor Element Binary Search

---
+++ 12_BinarySearch/FloorElementBS.java
@@ -0,0 +1,36 @@
+/*
+WAP: Floor Element Binary Search
+Date: 09-May-2026
+*/
+
+public class FloorElementBS {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 10, 10, 12, 19};
+        int x = 5;
+
+        int ans = floorElement(arr, x);
+
+        if(ans == -1){
+            System.out.print("The given target " + target + " floor value is not present!");
+        }else{
+            System.out.print("The given target " + target + " floor valie is present at " + ans + " index!");
+        }
+    }
+
+    public static int floorElement(int[] arr, int k){
+        int low=0, high=arr.length-1, indx = -1;
+
+        while(low<=high){
+            ind mid = low+(high-low)/2;
+
+            if(arr[mid] > k){
+                high = mid-1;
+            }else{
+                indx = mid;
+                low = mid+1;
+            }
+        }
+        return indx;
+    }
+}
+
